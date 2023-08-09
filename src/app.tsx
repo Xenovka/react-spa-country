dfb3530500c6a9c82e157bc757386da75ef4e0da
Xenovka <wendyanto18@gmail.com>
2023-08-09 07:03:35 +0700

    init project

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="text-center">
+      <h1 className="font-bold text-3xl">Hello world!</h1>
+    </div>
+  );
+}
