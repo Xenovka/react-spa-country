0b43a54dca5b1fdc70dff8ba9ca30a51cd72febe
Xenovka <wendyanto18@gmail.com>
2023-08-09 11:39:40 +0700

    change to grid for responsiveness

--- src/app.jsx
+++ src/app.jsx
@@ -43,7 +43,7 @@ export default function App() {
   return (
     <div className="p-6">
       <SearchBar setSearchResult={setSearchResult} countries={data.countries} />
-      <div className="flex flex-wrap gap-3 justify-between text-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5 gap-3 justify-center text-center">
         <CountryCard
           setClickedIndex={setClickedIndex}
           clickedIndex={clickedIndex}
