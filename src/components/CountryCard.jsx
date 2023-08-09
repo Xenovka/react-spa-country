51da2ec0ca48b5467027656550f252c0bd7d5345
Xenovka <wendyanto18@gmail.com>
2023-08-09 12:01:55 +0700

    improve styling with shadow

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -9,17 +9,19 @@ export default function CountryCard({ setClickedIndex, clickedIndex, countries }
         <div
           onClick={() => onCardClicked(index)}
           key={code}
-          className={`flex-1 min-w-[20%] h-52 card ${clickedIndex === index ? "flip" : ""}`}
+          className={`flex-1 min-w-[20%] h-52 hover:shadow-lg hover:shadow-indigo-100 transition-shadow bg-slate-100 rounded-md card ${
+            clickedIndex === index ? "flip" : ""
+          }`}
         >
           <div className="relative w-full h-full p-2 card-wrapper">
-            <div className="flex flex-col justify-center bg-slate-200 card-front text-left p-4">
+            <div className="flex flex-col justify-center  card-front text-left p-4">
               <p className="text-6xl text-center">{emoji}</p>
               <h1 className="text-center font-bold text-xl mb-2">{name}</h1>
               <p>Capital: {capital || "N/A"}</p>
               <p>Currency: {currencies[0] || "N/A"}</p>
             </div>
-            <div className="flex flex-col justify-center bg-slate-400 card-back">
-              <p className="text-center text-base px-2 text-white font-semibold">
+            <div className="flex flex-col justify-center card-back">
+              <p className="text-center text-base px-2 font-semibold">
                 This country is located in the continent of {continent.name}. The people in this country speak{" "}
                 {languages.length ? languages[0].name : "English"} as their native language.
               </p>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,7 +8,7 @@ export default function SearchBar({ setSearchResult, countries }) {
   return (
     <div className="w-1/2 h-10 mx-auto gap-2 mb-6">
       <input
-        className="border-2 focus:outline-none rounded-md w-full p-2 bg-slate-200"
+        className="border-2 focus:outline-none rounded-md w-full p-2 bg-slate-200 placeholder-slate-400 focus:shadow-md focus:shadow-indigo-100 transition-shadow duration-300 border-none"
         type="text"
         placeholder="Search Country..."
         onChange={handleSearchInput}
