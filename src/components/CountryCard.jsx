d531654573ee5d2f5f456c0005115cb2ba28cf96
Xenovka <wendyanto18@gmail.com>
2023-08-09 10:25:42 +0700

    improve styling and change details text

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -18,13 +18,15 @@ export default function CountryCard({ countries }) {
           <div className="relative w-full h-full p-2 card-wrapper">
             <div className="flex flex-col justify-center bg-slate-200 card-front text-left p-4">
               <p className="text-6xl text-center">{emoji}</p>
-              <h1 className="text-center font-bold text-xl">{name}</h1>
+              <h1 className="text-center font-bold text-xl mb-2">{name}</h1>
               <p>Capital: {capital || "N/A"}</p>
               <p>Currency: {currencies[0] || "N/A"}</p>
             </div>
             <div className="flex flex-col justify-center bg-slate-400 card-back">
-              <h1>{continent.name}</h1>
-              <h1>{languages.length <= 0 ? "N/A" : languages[0].name}</h1>
+              <p className="text-center text-base px-2 text-white font-semibold">
+                This country is located in the continent of {continent.name}. The people in this country speak
+                {languages ? " English" : languages[0].name} as their native language.
+              </p>
             </div>
           </div>
         </div>
